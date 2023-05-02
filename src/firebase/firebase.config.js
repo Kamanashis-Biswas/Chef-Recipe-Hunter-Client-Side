639669eb7972d38afd3b5c5c2e37b19b66437954
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-05-02 12:01:14 +0600

    Component setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8QdSm1tvXKdKQnBTHWdoSVO1lt-sgDVw",
+  authDomain: "ph-assignment-10-567b3.firebaseapp.com",
+  projectId: "ph-assignment-10-567b3",
+  storageBucket: "ph-assignment-10-567b3.appspot.com",
+  messagingSenderId: "800374655983",
+  appId: "1:800374655983:web:52877e35f7ce717e95841a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,18 +1,19 @@
-import "./App.css";
+/* eslint-disable no-unused-vars */
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import React from 'react';
 
-
-function App() {
-  return (
-    <>
-      {/* <Navbar fluid={true} rounded={true}>
-        <Navbar.Brand href="https://flowbite.com/">
+const Header = () => {
+    return (
+        <div>
+            <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="/">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="https://i.ibb.co/415RpMN/Untitled-1.png"
             className="mr-3 h-6 sm:h-9"
             alt="Flowbite Logo"
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite
+          Chef Corner
           </span>
         </Navbar.Brand>
         <div className="flex md:order-2">
@@ -33,9 +34,7 @@ function App() {
                 name@flowbite.com
               </span>
             </Dropdown.Header>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>Earnings</Dropdown.Item>
+            
             <Dropdown.Divider />
             <Dropdown.Item>Sign out</Dropdown.Item>
           </Dropdown>
@@ -46,13 +45,11 @@ function App() {
             Home
           </Navbar.Link>
           <Navbar.Link href="/navbars">About</Navbar.Link>
-          <Navbar.Link href="/navbars">Services</Navbar.Link>
-          <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-          <Navbar.Link href="/navbars">Contact</Navbar.Link>
+          
         </Navbar.Collapse>
-      </Navbar> */}
-    </>
-  );
-}
+      </Navbar>
+        </div>
+    );
+};
 
-export default App;
+export default Header;
