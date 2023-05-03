b9e680c9eb61ab005e7ab8162ae7ba6d0b57315a
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-05-03 21:24:19 +0600

    Receipe Details Wirking

--- src/pages/ReceipeDetails/ReceipeDetails.jsx
+++ src/pages/ReceipeDetails/ReceipeDetails.jsx
@@ -3,17 +3,29 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ReceipeDetails = () => {
-  const { id  } = useParams();
+  const { id } = useParams();
   const [chef, setChef] = useState(null);
-  useEffect(()=>{
-    fetch(`http://localhost:5000/chef_details/${id}`).then(data=>data.json()).then(data=>{
-      setChef(data);
-    })
+  useEffect(() => {
+    fetch(`http://localhost:5000/chef_details/${id}`)
+      .then((data) => data.json())
+      .then((data) => {
+        setChef(data);
+      });
   }, []);
+
   return (
     <div>
       <div className="container mx-auto">
-        <h2>{chef && chef.chef}</h2>
+        <div className="border p-5 mt-10">
+          <img src={chef && chef.picture} alt="Chef Image" />
+          <h2>{chef && chef.chef}</h2>
+          <p>{chef && chef.short_bio}</p>
+          <p>Like: {chef && chef.likes}</p>
+          <p>ingredients:</p>
+          {chef && chef.ingredients?.map((item, i) => <li key={i}>{item}</li>)}
+          <p>Instructions:</p>
+          {chef && chef.instructions?.map((item, i)=><li key={i}>{item}</li>)}
+        </div>
       </div>
     </div>
   );
